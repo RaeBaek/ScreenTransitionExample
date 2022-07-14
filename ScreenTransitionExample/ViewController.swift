47e81083a0c3b647b1f6837eceb85cdc1cc3ffd0
RaeHoon Baek <bmp117@naver.com>
2022-07-14 14:23:21 +0900

    Delegate 패턴을 활용한 값 넘기기 응용

--- ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendDataDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,7 +18,10 @@ class ViewController: UIViewController {
 
     @IBAction func tapCodePushButton(_ sender: UIButton) {
         
-        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") as? CodePushViewController else { return }
+        
+        viewController.name = "RaeHoon"
+//        viewController.delegate = self
         
         self.navigationController?.pushViewController(viewController, animated: true)
         
@@ -24,12 +29,25 @@ class ViewController: UIViewController {
     
     @IBAction func tapCodePresentButton(_ sender: UIButton) {
         
-        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePresentViewController") else { return }
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePresentViewController") as? CodePresentViewController else { return }
         
         viewController.modalPresentationStyle = .fullScreen
+        viewController.name = "RaeHoon"
+        viewController.delegate = self
         self.present(viewController, animated: true, completion: nil)
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? SeguePushViewController {
+            viewController.name = "RaeHoon"
+        }
+    }
+    
+    
+    func sendData(name: String) {
+        self.nameLabel.text = name
+        self.nameLabel.sizeToFit()
+    }
 }
 
